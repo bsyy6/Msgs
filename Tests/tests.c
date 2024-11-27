a625fefea357e15c7b59dcce759510067492a764
waleed <w.alghilan@gmail.com>
2024-11-27 11:07:22 +0100

    WIP

--- Tests/tests.c
+++ Tests/tests.c
@@ -64,6 +64,30 @@ uint8_t runTest_2(uint8_t* test, uint8_t sizeTest){
     return msg.raw_buffer->msgCount;
 }
 
+uint8_t runTest_3(uint8_t* test, uint8_t sizeTest){
+    // loads the data one byte at a time and processes it
+    // adds some sum function to check it is correct!
+    uint8_t raw_buffer[20];
+    Buffer b_buffer = initBuffer(raw_buffer,20);
+    Msg msg = msg; // set a new message
+    
+    initMsg(&msg, &b_buffer);
+    setMsgSize(&msg, 10);
+    uint8_t startFlag[2] = {161,162};
+    uint8_t endFlag[2] = {162,161};
+    
+    addValidation(&msg, startFlag, 2);
+    addValidationFunction(&msg, myValidation);
+    addValidation(&msg, endFlag, 2);
+
+    for (int i = 0; i < sizeTest; i++){
+        enq(&test[i],&b_buffer);
+        processMsg(&msg);   
+    }
+
+    return msg.raw_buffer->msgCount;
+}
+
 validOutput myValidation(uint8_t byte ,uint8_t* t1,uint8_t t2, Buffer* buffer){
     static uint8_t prevByte = 0;    
     if(prevByte == 162){
